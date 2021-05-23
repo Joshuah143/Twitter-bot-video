2e3f422efb8edff69c0ab4016f4ba8971ee36468
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-05-23 14:09:53 -0600

    Setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('test')
+###
+# Planning:
+# Send tweet
+# get day of the week
+# formulate tweet
+# send email
+# send tweet
+# main functiom to put it all together
+# create config file or just use this
+###
+
