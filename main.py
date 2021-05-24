5dac827567ea7fff1f5cad92a22cbabdeb0198b8
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-05-23 23:55:55 -0600

    Setup

--- main.py
+++ main.py
@@ -4,10 +4,7 @@
 
 import json
 import random
-print("test")
-with open('qoutes.json', 'r') as file:
-    x = json.load(file)
-
-xyz = random.randint(1, len(x))
-
-print(f"{x[xyz]['quote']} \n- {x[xyz]['author']}")
+import twilio
+import smtplib
+import tweepy
+import time
