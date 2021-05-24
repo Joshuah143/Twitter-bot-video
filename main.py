fae8b072ddab4c0bdd0f71d61eb288adcd8c4ef6
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-05-23 20:23:49 -0600

    Setup

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('test')
+import json
+import random
+
+with open('qoutes.json', 'r') as file:
+    x = json.load(file)
+
+xyz = random.randint(1, 1000)
+
+print(f"{x[xyz]['quote']} \n- {x[xyz]['author']}")
